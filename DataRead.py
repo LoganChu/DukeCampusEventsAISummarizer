56d58f27f8f17895144b9d63c4440f58255df4e8
Logan Chu <logan.chu@duke.edu>
2024-11-18 10:59:10 -0500

    Added login functionality with Duke authentication

--- DataRead.py
+++ DataRead.py
@@ -3,6 +3,11 @@ import requests as rq
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+#Load environment variables
+load_dotenv()
 
 now = datetime.now()
 
@@ -24,12 +29,47 @@ with open("data.txt", "w", encoding="utf-8") as file:
 
         # Navigate to a URL
         page.goto(url)
-        time.sleep(3)
+        time.sleep(5)
 
         # Get the rendered page content
         html_content = page.content()
         #Parse HTML Content
         soup = BeautifulSoup(html_content, "html.parser")
+
+        #Login to get access to private locations
+        login = soup.find("a",{"aria-label":"Sign In Section"}).get('href')
+        page.goto("https://duke.campusgroups.com"+login)
+
+        time.sleep(5)
+
+        #Login Page Navigation
+        login_content = page.content()
+        soup_login = BeautifulSoup(login_content,'html.parser')
+
+        #Go to Authentication page
+        login = soup_login.find("a",class_="btn btn--school btn--full-width").get('href')
+        page.goto(login)
+
+        time.sleep(5)
+
+        #Getting username and password for .env file
+        username = os.getenv("DUKE_USERNAME")
+        password = os.getenv("DUKE_PASSWORD")
+
+        # Input username and password
+        page.fill("input#j_username", username) 
+        page.fill("input#j_password", password)
+
+        time.sleep(5)
+        # Click the login button
+        page.click("button#Submit")
+
+        """
+        # Wait for navigation or other post-login actions
+        page.wait_for_url("expected_url")  
+        """
+
+        time.sleep(5)
 
         #Section with all separate event pages
         content = soup.find("ul",id="divAllItems",class_="list-group")
@@ -48,7 +88,7 @@ with open("data.txt", "w", encoding="utf-8") as file:
             soup = BeautifulSoup(contents, "html.parser")
             description = " ".join((soup.find("div",id="event_details").find("div",class_="card-block").get_text()).split())
             file.write(description + "\n")
-            time.sleep(3)
+            time.sleep(100)
             index+=1
 
         # Close the browser
@@ -68,5 +108,3 @@ else:
     print("Failed to retrieve the page")
     exit()
 """
-
- 
